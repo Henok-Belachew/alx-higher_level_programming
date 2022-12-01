07352a43d9d0a00c6b3f90496375fd179bc33437
Henok Belachew <henokbelachewstu@gmail.com>
2022-12-01 15:09:33 +0000

    error fixed

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if (ord(c) >= 97) and (ord(c) <= 122):
-        print("{} is lower".format(c))
-    if (ord(c) >= 65 and (ord(c) <= 90):
-        print("{} is upper".format(c)
+    if ord(c) >= 90 and ord(c) <= 122:
+        return True
+    if ord(c) >= 65 and (ord(c) <= 90):
+        return False
