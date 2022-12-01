fd17161ad76c1d957c1ef70060c7bf0cbf219992
Henok Belachew <henokbelachewstu@gmail.com>
2022-12-01 14:11:10 +0000

    final commit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,7 +3,4 @@ for i in range(0, 100):
     if i == 99:
         print("{}".format(i))
     else:
-        if i < 10:
-            print("0{}, ".format(i), end="")
-        else:
-            print("{}, ".format(i), end="")
+        print("{:0>2d}, ".format(i), end="")
