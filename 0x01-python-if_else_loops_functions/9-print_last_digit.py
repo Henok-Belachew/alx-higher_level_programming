b7a492be589267b4ebc52806f45ea25a5e2968cb
Henok Belachew <henokbelachewstu@gmail.com>
2022-12-03 14:16:34 +0000

    betty fixed

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -5,6 +5,6 @@ def print_last_digit(number):
     if number < 0:
         lastdigit = ((-number) % 10)
     else:
-        lastdigit = number%10
+        lastdigit = number % 10
     print("{}".format(lastdigit), end="")
     return lastdigit
