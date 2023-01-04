7769dd5471119553ad26f4d220f4fed63c2105e6
Henok Belachew <henokbelachewstu@gmail.com>
2023-01-04 17:07:06 +0300

    task 5 edited

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,23 +1,50 @@
 #!/usr/bin/python3
 """
-This module defines `text_indentation`
+This module contains the functions that deal with formated printing.
 
-The function prints a text with 2 new lines after each of
-these characters: ., ? and :
+Functions:
+    text_indentation(text):
+        prints text by splitting it with this '?', '.', ':' delimeters.
+        when on of this delimeter is found 2 new lines are printed after
+        it any white space is removed after it.
 """
 
 
 def text_indentation(text):
-    """adds paragraph after `.`, `:` and `?`
+    """ text_indentation : function.
+    This function prints text by splitting it with this '?', '.', ':'
+    delimeters. When on of this delimeter is found 2 new lines are printed
+    after it any white space is removed after it.
 
     Args:
-        text (str)
-    """
-    if not isinstance(text, str):
-        raise TypeError('text must be a string')
+        text (str): The string to format
 
-    for l in '.:?':
-        text = text.replace(l, '{}\n'.format(l))
-    lines = text.splitlines()
-    for index, line in enumerate(lines):
-        print(line.strip(), end='' if index == len(lines) - 1 else '\n\n')
+    Return:
+        you can uncomment the return statment if you need it for any reason
+        I added it for testing the function. but by default it does not
+        return any thing.
+    """
+    toPrint = ""
+    flag = False
+    spaces = length = i = 0
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    for j in reversed(text):
+        if j in ' \t':
+            spaces += 1
+        else:
+            break
+    length = len(text) - spaces
+    while i < length:
+        if text[i] not in ' \t':
+            flag = True
+        if flag:
+            toPrint += text[i]
+        if text[i] in '?.:':
+            toPrint += '\n\n'
+            flag = False
+        i += 1
+    print(toPrint, end='')
+    # if you want you can uncommnet the return statment.
+    # return (toPrint)
