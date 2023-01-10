ce696d67afea5c0058d899471c31a4e189052860
Henok Belachew <henokbelachewstu@gmail.com>
2023-01-11 01:15:06 +0300

    another error fixed

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
-"""This module defines a class Student"""
+"""json"""
+
 
 class Student:
-    """Represent a student."""
+    """A student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-        def to_json(self, attrs=None):
-            """Gets a dictionary representation of the Student.
-            If attrs is a list of strings, represents only those attributes
-            included in the list
-            """
-            if (type(attrs) == list and
-                    all(type(ele) == str for ele in attrs)):
-                return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of a Student instance."""
+        if attrs is not None and all(isinstance(x, str) for x in attrs):
+            d = {}
+            for k, v in self.__dict__.items():
+                if k in attrs:
+                    d[k] = v
+            return d
+        else:
             return self.__dict__
 
-        def reload_from_json(self, json):
-            """Replaces all attributes of the Student
-            """
-            for k, v in json.items():
-                setattr(self, k, v)
+    def reload_from_json(self, json):
+        """reload data from json"""
+        for (key, value) in json.items():
+            setattr(self, key, value)
