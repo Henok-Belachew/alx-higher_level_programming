09ba32e15686d11c31d62cdd097f383144284f90
Henok Belachew <learning4myfuture@gmail.com>
2023-02-27 03:50:34 -0800

    fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for x in range(1,101):
+        if x == 100:
+            print("Buzz")
+        elif x % 3 == 0:
+            print("Fizz", end=" ")
+        elif x % 5 == 0:
+            print("Buzz", end=" ")
+        elif x % 3 == 0 and x % 5 == 0:
+            print("FizzBuzz", end=" ")
+        else:
+            print("{}".format(x), end=" ")            
