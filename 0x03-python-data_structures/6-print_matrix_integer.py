b6bd8374ec5089e71105a92131332c89ef621b88
Henok Belachew <learning4myfuture@gmail.com>
2023-03-03 23:34:01 -0800

    printed lists of list

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+
+    for m in range(0, len(matrix)):
+        for n in range(0, len(matrix[m])):
+            sepa = "\n" if n + 1 == len(matrix[m]) else " "
+            print("{}".format(matrix[m][n]), end=sepa)
