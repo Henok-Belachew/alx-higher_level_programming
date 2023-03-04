cf7745494813e664dff8607dba2b244919c3b7ac
Henok Belachew <learning4myfuture@gmail.com>
2023-03-03 23:57:21 -0800

    error fixed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,8 +4,9 @@
 def print_matrix_integer(matrix=[[]]):
 
     if not matrix:
-        print('')
-    for m in range(0, len(matrix)):
-        for n in range(0, len(matrix[m])):
-            sepa = "\n" if n + 1 == len(matrix[m]) else " "
-            print("{:d}".format(matrix[m][n]), end=sepa)
+        print()
+    else:
+        for m in range(0, len(matrix)):
+            for n in range(0, len(matrix[m])):
+                sepa = "\n" if n + 1 == len(matrix[m]) else " "
+                print("{:d}".format(matrix[m][n]), end=sepa)
