fa903e2e99084e5729c93d29e5452d32f80e16ce
Henok Belachew <learning4myfuture@gmail.com>
2023-03-05 01:41:56 -0800

    the del keyword

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    newList = []
+
     if idx >= len(my_list) or idx < 0:
         return my_list
     for i in range(0, len(my_list)):
         if i == idx:
-            continue
+            del my_list[i]
         else:
-            newList.append(my_list[i])
-    return newList
+            continue
+    return my_list
