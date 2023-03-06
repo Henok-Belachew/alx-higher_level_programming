8c34ef1a6053150baf31a31f6e763038cacf7b4f
Henok Belachew <learning4myfuture@gmail.com>
2023-03-06 07:16:41 -0800

    empty list considered

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,6 +3,8 @@
 def weight_average(my_list=[]):
     add = 0
     mulAdd = 0
+    if my_list == []:
+        return 0
     for i in range(0, len(my_list)):
         add = add + my_list[i][1]
         mulAdd = mulAdd + (my_list[i][0] * my_list[i][1])
