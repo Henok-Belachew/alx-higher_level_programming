a21ed2686e107eec369d19688844db897c41f087
Henok Belachew <learning4myfuture@gmail.com>
2023-03-09 13:19:38 -0800

    has to workgit status

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,7 +2,7 @@
 
 def safe_function(fct, *args):
     try:
-        result = fct(*args)
+        result = fct(args[0], args[1])
         return result
     except Exception as err:
         print("Exception: {}".format(err))
