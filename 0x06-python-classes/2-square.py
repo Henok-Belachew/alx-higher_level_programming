5ab2f3eb3add70dcd3b98b1fb47a75c0dda9e2f2
Henok Belachew <learning4myfuture@gmail.com>
2023-03-11 02:32:30 -0800

    bug fixed

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,9 +8,8 @@ class Square:
     """This class is a squeleton for Square
     """
     def __init__(self, size=0):
-        try:
-            self.__size = int(size)
-            if size < 0:
-                raise ValueError("size must be >= 0")
-        except TypeError:
-            raise TypeError("size must be an integer")
+        self.__size = size
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
