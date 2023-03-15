b615d9158737d02d4a1aa5891b59714555d62c27
Henok Belachew <henokbelachewstu@gmail.com>
2023-03-15 20:27:10 +0300

    bug fixed

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const newList = list.map(function (num, index) {
-  return num * index;
-});
-
+const mapped = list.map(function (value, index) { return value * index; });
 console.log(list);
-console.log(newList);
+console.log(mapped);
