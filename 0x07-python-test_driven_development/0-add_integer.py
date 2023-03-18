1b1aca8c2c57e73b9c33e8d66a4fd239284d82a9
Henok Belachew <learning4myfuture@gmail.com>
2023-03-18 11:11:12 -0700

    python code tested

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+This module defines `add_integer`
+The function returns the sum of a and b
+"""
+
+
+def add_integer(a, b=98):
+
+    """
+    Returns the sum of the two argument passed
+
+    Args:
+        a(int or float): an integer or float
+        b(int or float): has defulat value of 98
+
+    Returns:
+        int: the integer sum of the arguments passed
+
+    Raises:
+        TypeError: if a or b is not float or int
+
+    Examples:
+        >>> add_integer(5, 9)
+        14
+        >>> add_integer(5.9, 9.8)
+        15
+        >>> add_integer(-1, -5)
+        -6
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+
+    return int(a + b)
+    
