a43f8e23d0fcffaa43ee83aaea6dcbed9f9c52fe
Henok Belachew <henokbelachewstu@gmail.com>
2023-03-19 14:11:50 +0300

    code modefied

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,37 +1,33 @@
 #!/usr/bin/python3
-
-"""
-This module defines `add_integer`
-The function returns the sum of a and b
-"""
+'''This module adds two integers.
+It contains 1 function ``add_integer``'''
 
 
 def add_integer(a, b=98):
-
-    """
-    Returns the sum of the two argument passed
-
-    Args:
-        a(int or float): an integer or float
-        b(int or float): has defulat value of 98
-
-    Returns:
-        int: the integer sum of the arguments passed
-
+    '''
+    Adds two integers
+    ARGS:
+    a - the first number
+    b - the second number
+    Returns: the result of the addition of a and b
+    Try it with 2 integers
+    >>> add_integer(2, 5)
+    7
+    It also works with floating point numbers,
+    but the result is always an integer.
+    For example:
+    >>> add_integer(7.0, 2.67)
+    9
     Raises:
-        TypeError: if a or b is not float or int
+        A TypeError for non int or non float datatypes
+    '''
 
-    Examples:
-        >>> add_integer(5, 9)
-        14
-        >>> add_integer(5.9, 9.8)
-        15
-        >>> add_integer(-1, -5)
-        -6
-    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-
-    return int(a + b)  
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return a + b
