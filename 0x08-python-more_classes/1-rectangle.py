8407b8fa9ce6c69a1907dceefef32978b3a0dcc1
Henok Belachew <learning4myfuture@gmail.com>
2023-03-19 07:41:42 -0700

    function documeted

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,17 +1,39 @@
 #!/usr/bin/python3
 """ 0-rectangel : module
+contains a class defination and more related to rectangles.
+Class:
+    Rectangle: used to define a Rectangle object.
 """
 
 
 class Rectangle:
-    """ Sekeleton for Rectangle Class
+    """ Rectangle: class.
+    This module contains a definations of A rectangle objects
+    and methods to act upon them.
+    Args:
+        width (optional: int): The widht to instantiate the rectangle object
+                               with if not provided it's defaulted to Zero(0).
+        height (optional: int): The height  to instantiate the rectangle object
+                                with if not provided it's defaulted to Zero().
+    Attributes:
+        width (int): widht of the rectangle object.
+        height (int): height of the rectangle object.
     """
+
     def __init__(self, width=0, height=0):
         self._width = width
         self._height = height
 
     @width.setter
     def width(self, value):
+        """width: property.
+        setter property for the privated instance attribute __width.
+        Args:
+            value (int): The value to set to the private instance attribute.
+                         __widht.
+        Reuturn:
+            None.
+        """
         if type(value) != int:
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -20,6 +42,14 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """width: property.
+        setter property for the privated instance attribute __height.
+        Args:
+            value (int): The value to set to the private instance attribute.
+                         __height.
+        Reuturn:
+            None.
+        """
         if type(value) != int:
             raise TypeError("height must be an integer")
         elif value < 0:
