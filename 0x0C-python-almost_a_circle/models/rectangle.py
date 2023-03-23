80b9f1cd31b3821e060524594d2d0ca03d102af7
Henok Belachew <learning4myfuture@gmail.com>
2023-03-23 07:40:28 -0700

    changed to private vars

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -18,8 +18,8 @@ class Rectangle(Base):
         super().__init__(id)
         self.__width = width
         self.__height = height
-        self.x = x
-        self.y = y
+        self.__x = x
+        self.__y = y
 
     @property
     def width(self):
@@ -49,22 +49,22 @@ class Rectangle(Base):
     def x(self):
         """Getter for x"""
 
-        return self.x
+        return self.__x
 
     @x.setter
     def x(self, value):
         """Setter for x"""
 
-        self.x = x
+        self.__x = x
 
     @property
     def y(self):
         """Getter for y"""
 
-        return self.y
+        return self.__y
 
     @y.setter
     def y(self, value):
         """Setter for Y"""
 
-        self.y = value
+        self.__y = value
