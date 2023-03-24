59a490ea54ac10dcd14ff094c31e9dc07a25a4bc
Henok Belachew <learning4myfuture@gmail.com>
2023-03-24 03:28:14 -0700

    will work

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,6 +20,9 @@ class Base:
         else:
             self.id = id
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """to json string converter"""
+        if not list_dictionaries:
+            return "[]"
         return json.dumps(list_dictionaries)
