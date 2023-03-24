f87b95ff53f44af43452d4601d04abefa25d3702
Henok Belachew <learning4myfuture@gmail.com>
2023-03-24 02:07:31 -0700

    square model added

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Square Class child of Rectangle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Squaer class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """constructor of square class"""
+
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        str1 = "[Square] ({}) {}/".format(self.id, self.x)
+        str2 = "{} - {}".format(self.y, self.size)
+        return (str1 + str2)
