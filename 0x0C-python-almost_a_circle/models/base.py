2dce526c851c02adca5a8c56098eba9a1e8dcfe5
Henok Belachew <learning4myfuture@gmail.com>
2023-03-24 21:58:04 -0700

    returned

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -49,7 +49,10 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """create a new object from dictionary"""
-        new = cls(10)
+        if cls.__name__ == "Rectangle":
+            new = cls(10, 10)
+        elif cls.__name__ == "Square":
+            new = cls(10)
 
         new.update(**dictionary)
         return new
