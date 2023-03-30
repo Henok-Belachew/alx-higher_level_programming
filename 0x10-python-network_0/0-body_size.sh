fffaa0f721f49adcb23f36e671f21de817e17e14
Henok Belachew <henokbelachewstu@gmail.com>
2023-03-30 21:41:38 +0300

    may this work!

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | grep 'Content-Length:' | cut -d' ' -f2
+# Takes in a URL, sends a GET req, displays the size of the body of the response
+curl --head -s "$1" | grep "Content-Length" | cut -d " " -f2;
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL and displays all HTTP methods the server will accept
-curl -sI "$1" | grep "Allow:" | sed -ne 's/^Allow: //p'
+# Takes in a URL and displays all HTTP methods the server will accept
+curl -s --head "$1" | grep "Allow" | cut -d " " -f2-;
