ec800744c6dadc1064a3a886da4dd75ababe1725
Henok Belachew <henokbelachewstu@gmail.com>
2023-05-05 02:48:56 +0300

    d

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,21 @@
 #!/usr/bin/node
+// prints the number of movies where the character “Wedge Antilles” is present.
 const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/';
 
-const url = process.argv[2];
-
-request(url, (err, res, body) => {
-  if (err) {
-    return console.log(err);
-  }
-
-  let count = 0;
-  const parsedBody = JSON.parse(body).results;
-
-  for (let i = 0; i < parsedBody.length; i++) {
-    const a = parsedBody[i].characters.find((c) => {
-      return c.match(/18/);
-    });
-    if (a !== undefined) {
-      count++;
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const film of films) {
+      for (const character of film.characters) {
+        if (character.includes('18')) {
+          count++;
+        }
+      }
     }
+    console.log(count);
   }
-
-  console.log(count);
 });
